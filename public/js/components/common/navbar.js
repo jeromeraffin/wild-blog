198a9b9cdd16059b799ac0545f1c33e91b1e9574
Jerome Raffin <raffin.jerome@gmail.com>
2017-05-03 11:12:07 +0200

    clock is working and refresh every seconds

--- public/js/components/common/navbar.js
+++ public/js/components/common/navbar.js
@@ -1,7 +1,16 @@
+import moment from 'moment';
+
 let navbar = {
     templateUrl: 'js/components/common/navbar.html',
-    controller: ['UsersService', '$state', function(UsersService, $state) {
+    controller: ['UsersService', '$state', '$interval', function(UsersService, $state, $interval) {
         'use strict'
+
+        this.date = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+        $interval(() => {
+          this.date = moment().format('MMMM Do YYYY, h:mm:ss a');
+        }, 1000)
+
         angular.extend(this, {
             $onInit() {
                 UsersService.getCurrent().then((user) => {
