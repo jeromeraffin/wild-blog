4d37587a2cd0a42ed54b3ef841c8aaeadf2e869f
Jerome Raffin <raffin.jerome@gmail.com>
2017-05-03 20:04:17 +0200

    bookmark works fine

--- public/js/components/blog/blogItem/blogItem.js
+++ public/js/components/blog/blogItem/blogItem.js
@@ -87,27 +87,28 @@ let blogItem = {
 
         this.isFav = () => {
             if (!this.post) return
-            return (this.user.bookmarks.find((post_id) => post_id.id === this.post._id))
+            return (this.user.bookmarks.find((post_id) => post_id == this.post._id))
         }
 
         this.addOrRemoveToBookmark = () => {
             // Try to find post in bookmarks
-            let postFound = this.user.bookmarks.find((post) => post.id === this.post._id)
+            let postFound = this.user.bookmarks.find((post_id) => post_id == this.post._id)
 
             if (!postFound) {
                 //Not found
                 this.user.bookmarks.push(this.post._id)
             } else {
                 //Found
-                this.user.bookmark = this.user.bookmarks.filtered((post_id) => {
-                    return post_id !== this.post._id
+                this.user.bookmarks = this.user.bookmarks.filter((post_id) => {
+                    return post_id != this.post._id
                 })
             }
 
             UsersService.update(this.user).then((res) => {
-                //return UsersService.setToken(res.data.token)
+                return UsersService.setToken(res.data.token)
             }).then((user) => {
-                Materialize.toast((postFound ? 'Removed' : 'Added'), 2000, (postFound ? 'toast-warning' : 'toast-success'))
+                let postFound = user.bookmarks.find((post_id) => post_id == this.post._id)
+                Materialize.toast((postFound ? 'Added' : 'Removed'), 2000, (postFound ? 'toast-success' : 'toast-warning'))
             }).catch((err) => {
                 let toastContent = `Error : ${err.data} !`
                 Materialize.toast(toastContent, 4000, 'toast-error')
